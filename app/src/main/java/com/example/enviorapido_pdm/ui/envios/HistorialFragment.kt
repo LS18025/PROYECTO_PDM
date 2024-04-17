d5ddd6662d9f4133868c8d3cb86c596434d8a2d4
LS18025 <ls18025@ues.edu.sv>
2024-04-16 22:17:42 -0600

    historial envios

--- app/src/main/java/com/example/enviorapido_pdm/ui/envios/HistorialFragment.kt
+++ app/src/main/java/com/example/enviorapido_pdm/ui/envios/HistorialFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.enviorapido_pdm.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HistorialFragment : Fragment() {
 
@@ -27,6 +29,10 @@ class HistorialFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HistorialViewModel::class.java)
         // TODO: Use the ViewModel
+
+        // Ocultar el botón en el fragmento
+        val button: Button? = activity?.findViewById(R.id.button2)
+        button?.visibility = View.GONE
     }
 
 }
