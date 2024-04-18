371284f0e16cbf4cbb1790d5a14fedce0111636c
LF18010 <lf18010@ues.edu.sv>
2024-04-18 10:15:32 -0600

    cambios1

---
+++ app/src/main/java/com/example/enviorapido_pdm/ui/administracion/RegistrarUsuarioViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.enviorapido_pdm.ui.administracion
+
+import androidx.lifecycle.ViewModel
+
+class RegistrarUsuarioViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
