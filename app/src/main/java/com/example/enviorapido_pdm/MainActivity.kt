ecc2d562ddd30aa0e17abdc443cc3cf2a8c56637
LS18025 <ls18025@ues.edu.sv>
2024-04-18 16:54:54 -0600

    actualización de vistas

--- app/src/main/java/com/example/enviorapido_pdm/MainActivity.kt
+++ app/src/main/java/com/example/enviorapido_pdm/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // Agregar OnClickListener al botón
         button.setOnClickListener {
             // Navegar al fragmento RegistrarEnvioFragment
-            navController.navigate(R.id.pedidosEntregaFragment)
+            navController.navigate(R.id.historialCompletadosFragment)
         }
     }
 }
