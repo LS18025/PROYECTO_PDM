7bb94aa6cbc1cee6109ab15d0e842cd807102c56
LS18025 <ls18025@ues.edu.sv>
2024-04-18 19:04:41 -0600

    modulo envíos

--- app/src/main/java/com/example/enviorapido_pdm/MainActivity.kt
+++ app/src/main/java/com/example/enviorapido_pdm/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // Agregar OnClickListener al botón
         button.setOnClickListener {
             // Navegar al fragmento RegistrarEnvioFragment
-            navController.navigate(R.id.historialCompletadosFragment)
+            navController.navigate(R.id.listaPedidosFragment)
         }
     }
 }
