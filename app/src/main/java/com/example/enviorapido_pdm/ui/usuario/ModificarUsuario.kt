e558419f62f62309a5da13f7fcee067e1f02a38d
Guiller92 <rr10092@ues.edu.sv>
2024-05-09 23:50:44 -0600

    nuevo activity
    cree un nuevo activity para agregar las funciones y configurar los xml de las vista de ususario

--- app/src/main/java/com/example/enviorapido_pdm/ui/usuario/ModificarUsuario.kt
+++ app/src/main/java/com/example/enviorapido_pdm/ui/usuario/ModificarUsuario.kt
@@ -1,4 +1,12 @@
 package com.example.enviorapido_pdm.ui.usuario
 
-class ModificarUsuario {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.enviorapido_pdm.R
+
+class ModificarUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modificar_usuario)
+    }
 }
