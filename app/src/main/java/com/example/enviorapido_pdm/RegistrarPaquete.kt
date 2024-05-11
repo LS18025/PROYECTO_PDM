de23f36d79085362ae60088768891cc77a5ef393
LS18025 <ls18025@ues.edu.sv>
2024-05-10 20:07:27 -0600

    vista paquetes

---
+++ app/src/main/java/com/example/enviorapido_pdm/RegistrarPaquete.kt
@@ -0,0 +1,11 @@
+package com.example.enviorapido_pdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarPaquete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_paquete)
+    }
+}
