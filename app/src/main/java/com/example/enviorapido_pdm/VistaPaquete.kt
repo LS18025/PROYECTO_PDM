a37ceb82e98ef7088fa3105a9cee036b9bfc01f0
LS18025 <ls18025@ues.edu.sv>
2024-05-21 22:44:41 -0600

    Eliminar Paquete Funcional

--- app/src/main/java/com/example/enviorapido_pdm/VistaPaquete.kt
+++ app/src/main/java/com/example/enviorapido_pdm/VistaPaquete.kt
@@ -4,16 +4,21 @@
     import android.content.Intent
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
+    import android.util.Log
     import android.widget.Button
     import android.widget.ImageButton
+    import android.widget.Toast
     import androidx.recyclerview.widget.LinearLayoutManager
     import androidx.recyclerview.widget.RecyclerView
+    import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-    class VistaPaquete : AppCompatActivity(), PaqueteAdapter.OnItemClickListener {
+    class VistaPaquete : AppCompatActivity(), PaqueteAdapter.OnItemSelectedListener {
 
         private lateinit var recyclerView: RecyclerView
         private lateinit var adapter: PaqueteAdapter
         private lateinit var dbHelper: ConexionDataBaseHelper
+
+        private var selectedPaquete: Paquete? = null
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -21,22 +26,54 @@
 
             recyclerView = findViewById(R.id.listaPaquete)
             recyclerView.layoutManager = LinearLayoutManager(this)
-            adapter = PaqueteAdapter(ConexionDataBaseHelper(this).RecuperarTodoslosPaquetes())
+
+            adapter = PaqueteAdapter(ConexionDataBaseHelper(this).RecuperarTodoslosPaquetes(), this)
             recyclerView.adapter = adapter
 
+
+
             val btnAgregar: ImageButton = findViewById(R.id.btnAgregarPaquete)
+            val btnEditar: ImageButton = findViewById(R.id.btnEditarPaquete)
+            val btnEliminar: ImageButton = findViewById(R.id.btnBorrarPaquete)
 
-            btnAgregar.setOnClickListener()
-            {
-                 val Intent = Intent(this,AgregarPaquete::class.java)
-                 startActivity(Intent)
+            btnAgregar.setOnClickListener {
+                val intent = Intent(this, AgregarPaquete::class.java)
+                startActivity(intent)
             }
+
+            btnEditar.setOnClickListener {
+                selectedPaquete?.let {
+                    val intent = Intent(this, EditarPaquete::class.java)
+                    intent.putExtra("id_paquete", it.idPaquete)
+                    startActivity(intent)
+                }
+            }
+            btnEliminar.setOnClickListener {
+                if (selectedPaquete != null) {
+                    dbHelper = ConexionDataBaseHelper(this)
+                    val idPaquete = selectedPaquete!!.idPaquete
+                    val filasAfectadas = dbHelper.EliminarPaquete(idPaquete)
+                    if (filasAfectadas > 0) {
+                        // Eliminación exitosa, actualiza la lista de paquetes
+                        adapter.listaPaquete.remove(selectedPaquete!!)
+                        adapter.notifyDataSetChanged()
+                        selectedPaquete = null // Limpia la selección
+                    } else {
+                        // Error al eliminar, muestra un mensaje de error
+                        Toast.makeText(this, "Error al eliminar el paquete", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // No hay paquete seleccionado, muestra un mensaje de advertencia
+                    Toast.makeText(this, "Selecciona un paquete para eliminar", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+        }
+
+        override fun onItemSelected(paquete: Paquete) {
+            selectedPaquete = paquete
         }
 
 
-        override fun onItemClick(idPaquete: Int) {
-            val intent = Intent(this, EditarPaquete::class.java)
-            intent.putExtra("id_paquete", idPaquete)
-            startActivity(intent)
-        }
     }
